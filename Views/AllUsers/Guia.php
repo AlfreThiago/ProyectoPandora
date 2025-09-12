41465ca8cb10409e8cab246b0074ee4c76b870cb
DaniDom-41 <danielsierra0206@gmail.com>
2025-09-11 22:29:22 -0300

    se hizo la conexion al css con la guia

--- Views/AllUsers/Guia.php
+++ Views/AllUsers/Guia.php
@@ -1,11 +1,15 @@
 <head>
-    <link rel="stylesheet" href="Public/css/Includes/GuiaDash.css">
+    <link rel="stylesheet" href="../../Public/css/GuiaDash.css">
 </head>
 <main class="main-guia">
+    <title>Guide In Inglish</title>
     <h1 class="h1-guia">Guide About Innovasys</h1>
     <h2>In this section we explain how to use our repair service!</h2>
     <p class="p-guia">The first thing we have to do when we want to request our services would be to register on our page👇</p>
+    <br>
     <p class="p-guia">To register, we must fill out a form with our personal information such as name, email, and password.</p>
+    <br>
     <p class="p-guia">Once we have registered, we can log in to our account and access the dashboard where we can see all the services we offer.</p>
     <p class="p-guia">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a perferendis eaque exercitationem corporis dolorem officia odit accusantium, provident commodi neque aliquid consequatur excepturi eos omnis. Voluptas iure dolore ducimus.</p>
 </main>
+
